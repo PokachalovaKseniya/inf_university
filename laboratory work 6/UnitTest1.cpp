3dccb4d0d79d5652b0b9bd545b7077567f593b5a
PokachalovaKseniya <ksupro228@mail.ru>
2025-01-14 18:20:10 +0300

    .

---
+++ laboratory work 6/UnitTest1.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Ксения\Desktop\уник\информатика\проекты\дз1\дз1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		float x = 1 ;
+		float y = 3;
+		float PI = 3.141592653589793;
+
+		float R = R = 1 / tan(PI * x / y);
+		float S = pow(x + log(1 / x), 1 / 3);
+
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(R, 0.0f);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			Assert::AreEqual(S, 0.0f);
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			Assert::IsTrue(R>S);
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			Assert::IsTrue(S>R);
+		}
+
+		TEST_METHOD(TestMethod5)
+		{
+			Assert::IsTrue(R>0);
+		}
+
+		TEST_METHOD(TestMethod6)
+		{
+			Assert::IsTrue(S>0);
+		}
+	};
+}
---
+++ laboratory work 6/дз1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+int main()
+{
+    float x, y;
+    const double PI = 3.141592653589793;
+
+    std::cout << "X: ";
+    std::cin >> x;
+    std::cout << "Y: ";
+    std::cin >> y;
+
+    // Проверка на корректность ввода
+    if (x <= 0) {
+        std::cerr << "Error: x must be positive and non-zero." << std::endl;
+        return 1;
+    }
+
+    double R = 1 / tan(PI * x / y);
+    double S = pow(x + log(1 / x), 1 / 3);
+    double C = std::max(R, S);
+
+    std::cout << "R = " << R << "\n";
+    std::cout << "S = " << S << "\n";
+    std::cout << "C = " << C << "\n";
+
+    std::cout << "Press ENTER to close app.... ";
+    std::cin.ignore();
+    std::cin.get();
+    return 0;
+}
+
